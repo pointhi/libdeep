dc9efcea0d276c3bb852a018beeed5f0e6d51e93
Bob Mottram <bob@freedombone.net>
2017-07-15 20:32:10 +0100

    Comments

--- src/autocoder.h
+++ src/autocoder.h
@@ -6,13 +6,13 @@
  modification, are permitted provided that the following conditions
  are met:
  1. Redistributions of source code must retain the above copyright
-	notice, this list of conditions and the following disclaimer.
+    notice, this list of conditions and the following disclaimer.
  2. Redistributions in binary form must reproduce the above copyright
-	notice, this list of conditions and the following disclaimer in the
-	documentation and/or other materials provided with the distribution.
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
  3. Neither the name of the University nor the names of its contributors
-	may be used to endorse or promote products derived from this software
-	without specific prior written permission.
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
  .
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -39,48 +39,51 @@
 #include "backprop_neuron.h"
 
 struct autocode {
-	unsigned int random_seed;
+    unsigned int random_seed;
 
-	/* layer dimensions.
-	   Number of outputs is the same as number of inputs */
-	int NoOfInputs,NoOfHiddens;
+    /* layer dimensions.
+       Number of outputs is the same as number of inputs */
+    int NoOfInputs,NoOfHiddens;
 
-	float DropoutPercent;
+    float DropoutPercent;
 
-	/* layers */
-	float * inputs;
-	float * hiddens;
-	float * outputs;
+    /* layers */
+    float * inputs;
+    float * hiddens;
+    float * outputs;
 
-	/* weights. Note that these are symmetric.
-	   inputs -w-> hiddens <-w- outputs */
-	float * weights;
+    /* weights. Note that these are symmetric.
+       inputs -w-> hiddens <-w- outputs */
+    float * weights;
 
-	/* array used during learning */
-	float * lastWeightChange;
+    /* array used during learning */
+    float * lastWeightChange;
 
-	/* biases of hidden units */
-	float * bias;
-	float * lastBiasChange;
+    /* biases of hidden units */
+    float * bias;
+    float * lastBiasChange;
 
-	/* backprop error */
-	float * bperr;
-	float BPerror;
-	float BPerrorPercent;
-	float BPerrorAverage;
+    /* backprop error */
+    float * bperr;
+    float BPerror;
+    float BPerrorPercent;
+    float BPerrorAverage;
 
-	float learningRate;
-	float noise;
+    /* learning rate in the range 0.0 -> 1.0 */
+    float learningRate;
 
-	/* training itterations */
-	unsigned int itterations;
+    /* added noise in the range 0.0 -> 1.0 */
+    float noise;
+
+    /* training itterations */
+    unsigned int itterations;
 };
 typedef struct autocode ac;
 
 int autocoder_init(ac * autocoder,
-				   int no_of_inputs,
-				   int no_of_hiddens,
-				   unsigned int random_seed);
+                   int no_of_inputs,
+                   int no_of_hiddens,
+                   unsigned int random_seed);
 void autocoder_free(ac * autocoder);
 void autocoder_encode(ac * autocoder, float * encoded, unsigned char use_dropouts);
 void autocoder_decode(ac * autocoder, float * decoded);
@@ -97,10 +100,10 @@ void autocoder_update(ac * autocoder);
 void autocoder_normalise_inputs(ac * autocoder);
 int autocoder_compare(ac * autocoder0, ac * autocoder1);
 int autocoder_plot_weights(ac * autocoder,
-						   int feature_index,
-						   int patch_radius, int patch_depth,
-						   int img_tx, int img_ty, int img_bx, int img_by,
-						   unsigned char img[],
-						   int img_width, int img_height);
+                           int feature_index,
+                           int patch_radius, int patch_depth,
+                           int img_tx, int img_ty, int img_bx, int img_by,
+                           unsigned char img[],
+                           int img_width, int img_height);
 
 #endif
